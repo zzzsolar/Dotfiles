78bd6ed532f4ba012f60c79de511b3b8fd258971
Solar <110750401+zzzsolar@users.noreply.github.com>
2022-11-04 17:04:48 -0400

    Update config.h

--- DWM/config.h
+++ DWM/config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 15;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -14,16 +14,16 @@ static const char col_gray1[]       = "#2E3440";
 static const char col_gray2[]       = "#eceff4"; /* border color unfocused windows */
 static const char col_gray3[]       = "#eceff4";
 static const char col_gray4[]       = "#eceff4";
-static const char col_cyan[]        = "#81a1c1"; /* border color focused windows and tags */
+static const char col_cyan[]        = "#4c566a"; /* border color focused windows and tags */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_cyan },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2  },
 };
 
 /* tagging */
 /*static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };*/
-static const char *tags[] = { "", "", "", "", "嗢", "ﱘ", "", "", "" };
+static const char *tags[] = { "", "爵", "", "", "嗢", "ﱘ", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", "-z", "20", NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
 //static const char *firefoxcmd[]  = { "brave", " NULL };
 
